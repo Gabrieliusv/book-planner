62d73ccdd358f1bae695471379b2ba75ac88eba3
Gabrieliusv <gabrielius.svi@gmail.com>
2019-07-20 11:57:59 +0300

    Updates

---
+++ precache-manifest.ba5921b384e7a3be06bff762ee620f34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d5b7026757b17a204cea2b26c2d90ef",
+    "url": "/book-planner/index.html"
+  },
+  {
+    "revision": "54d1bfd0fd778558a4d5",
+    "url": "/book-planner/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "549d4a11adce1fdf19b7",
+    "url": "/book-planner/static/js/2.22da0c06.chunk.js"
+  },
+  {
+    "revision": "54d1bfd0fd778558a4d5",
+    "url": "/book-planner/static/js/main.5378cdfc.chunk.js"
+  },
+  {
+    "revision": "2d810f256186eeba9ced",
+    "url": "/book-planner/static/js/runtime~main.fe40ab46.js"
+  }
+]);
